c437d06f2853df95c16cdc6255af9f470dc4858e
Caio Lima <53834100+Nergal-code@users.noreply.github.com>
2024-09-25 12:07:05 -0300

    Update App.js
    Testando componentes.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import HeroBanner from './components/HeroBanner'
 import Menu from './components/header/Menu'
 import Body from './components/Body'
 
-
 const AppContainer = styled.div`
-  background-color: black; // Fundo escuro para contraste
-  height: 100vh;
+  background-color: black;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media (max-width: 768px) {
+    /* Responsividade para tablets e celulares */
+    background-color: darkgray;
+  }
 `;
 
-
-function App() {
+function App(): JSX.Element {
   return (
     <>
-    <GlobalCSS/>
-      <Menu/>    
-        <HeroBanner/>
-        <Body/>
+      <GlobalCSS />
+      <AppContainer>
+        <Menu />
+        <HeroBanner />
+        <Body />
+      </AppContainer>
     </>
   )
 }
