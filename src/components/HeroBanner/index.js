83bfc0c09a20f37c45508b99ca29af4024ffc1d4
havokkmorands <brunosaldanhamarinho@gmail.com>
2024-10-11 19:43:31 -0300

    :white_check_mark: improve: the banner have a text now!

--- src/components/HeroBanner/index.js
+++ src/components/HeroBanner/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Hero, Image } from './styled'
+import { Hero, Image, HeroContainer, HeroText } from './styled'
 import heroImg from '../../assets/images/hero/hero.jpg'
 
 const HeroBanner = () => (
 
     <Hero>
         <Image style={{ backgroundImage:`url(${heroImg})` }}>
+            <HeroContainer>
+                <HeroText>
+                    In a world where shadows whisper secrets and the blood moon rises, the line between hero and monster blurs, revealing the true darkness that lurks within every soul.
+                </HeroText>
+            </HeroContainer>
         </Image>
     </Hero>
 )
--- src/components/HeroBanner/styled.js
+++ src/components/HeroBanner/styled.js
@@ -16,3 +16,20 @@ export const Image = styled.div`
   background-position: center;
   background-repeat: no-repeat;
 `;
+
+export const HeroContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 10%;
+  display: flex;
+  width: 40%;
+  height: 30%;
+  justify-content: flex-end;
+  align-items: flex-start;
+  z-index: 1;
+`
+
+export const HeroText = styled.p`
+  color: white;
+  font-size: 3.1vh;
+`
